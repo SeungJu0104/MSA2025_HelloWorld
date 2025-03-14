fbd56787861a04b2d94f01295928452e8ab1ac81
SeungJu0104 <skatmdwn23@naver.com>
2025-03-14 15:45:09 +0900

    주석 변경

--- src/com/msa2025/Hello.java
+++ src/com/msa2025/Hello.java
@@ -1,7 +1,7 @@
 package com.msa2025;
 
 public class Hello {
-	// 패키지 익스플로러에서 프로젝트명 옆 []에 뭔가 써져있다면 git이 생성된 것이다.
+	// 패키지 익스플로러에서 프로젝트명 옆 []에 뭔가 써져있다면 git 로컬저장소가 생성된 것이다.
 	public static void main(String[] args) {
 		System.out.println("안녕하세요.");
 	}
